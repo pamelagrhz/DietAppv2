4b5ac3cc0ff5c7ac8fd35136ca7776d783afbc42
pamelagrhz <pamelagrhz@gmail.com>
2026-02-21 19:45:41 -0600

    [BE-branch] Set imports on index file and get with express the configured port configurations

---
+++ DietApp-BE/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+//Almacenar las configuraciones de CORS en una variable
+// const corsOptions = {
+//     origin: 'http://localhost:3000', // Permitir solo solicitudes desde este origen
+// }
+
+
+// Obtener con express el origen de la solicitud y regresar un json 
+// Endpoint que responde con un JSON
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+});
+//Configuración de express para escuchar en el puerto 3000
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
