9b619cdd7708f0a1e5c6389539f74e922f6f3e9b
pamelagrhz <pamelagrhz@gmail.com>
2026-03-01 13:00:12 -0600

    [BE-branch] Create BE get

--- DietApp/src/App.jsx
+++ DietApp/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   //hook use effect para hacer la petición a la API y guardar los datos en el estado
   useEffect(() => {
     api().then(data => {
-      setApiData(data.message);
+      setApiData(data.recetas);
     })
   }, []);
 
@@ -23,8 +23,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           useState for a counter {count}
         </button>
-        <h2>{apiData}</h2>
-        <RecipeList  recipes={recipe}/>
+        <h2>Recetas para {recipe.personas} persona(s) de: {recipe.porcion_base_KG} kg</h2>
+       <RecipeList  recipes={recipe}/>{/* Cambiar a apiData */}
       </div>
     </>
   )
--- DietApp/src/assets/api.js
+++ DietApp/src/assets/api.js
@@ -1,6 +1,9 @@
 //Crear una función asíncrona que haga una petición a la API y devuelva los datos en formato JSON
-export const api = async() => {
-    const response = await fetch('http://localhost:3000');
-    const data = response.json();
+export const api = async () => {
+    const response = await fetch('http://localhost:3000/recipes');
+    if (!response.ok) {
+        throw new Error('No se pudieron obtener las recetas');
+    }
+    const data = await response.json();
     return data;
 }
