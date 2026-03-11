20f94e482b762c8bd9b1b142397f39628505ae27
pamelagrhz <pamelagrhz@gmail.com>
2026-03-11 16:35:10 -0600

    [BE-branch] Document the BE new structure

--- DietApp-BE/app.js
+++ DietApp-BE/app.js
@@ -1,12 +1,12 @@
 //Configure express and middlewares
 import express from 'express';
-import cors from 'cors';
+import cors from 'cors'; 
 import recipesRoutes from './routes/recipes.routes.js';
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Middleware to parse JSON bodies
 
 //all the recipes routes will be prefixed with /recipes
 //use routes/recipes.routes.js for all the endpoints related to recipes
