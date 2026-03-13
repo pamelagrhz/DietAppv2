275cffe20354b75906167a092f3f010495880e23
pamelagrhz <pamelagrhz@gmail.com>
2026-03-12 18:25:37 -0600

    [BE-branch] Implement a proxy

--- DietApp/src/features/recipes/services/recipes.service.js
+++ DietApp/src/features/recipes/services/recipes.service.js
@@ -1,6 +1,6 @@
 //Crear una función asíncrona que haga una petición a la API y devuelva los datos en formato JSON
 export const getRecipes = async () => {
-    const response = await fetch('http://localhost:3000/recipes');
+    const response = await fetch('/api/recipes');
     if (!response.ok) {
         throw new Error('No se pudieron obtener las recetas');
     }
--- DietApp/vite.config.js
+++ DietApp/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    //Configure a proxy to forward API requests to the backend server
+    //For example: If call fetch('/api/recipes') in the frontend, it will be proxied to http://localhost:3000/recipes
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
