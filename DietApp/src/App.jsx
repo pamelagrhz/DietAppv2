c1d3669b8dca0454f8a0de8342f9b2aaf3b223ca
pamelagrhz <pamelagrhz@gmail.com>
2026-04-29 16:08:02 -0600

    [redesign-FE]Implement react routing

--- DietApp/src/App.jsx
+++ DietApp/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css'
 import Recipes from './features/showRecipes/pages/Recipes.jsx';
 import AppHeader from './features/addRecipes/createRecipeModal.jsx';
@@ -7,10 +8,14 @@ import HeaderMenu from './components/headerMenu.jsx';
 
 function App() {
   const [isCreateRecipeOpen, setCreateRecipeOpen] = useState(false);
+  const navigate = useNavigate();
 
   const handleMenuOptionClick = (action) => {
     if (action === 'newRecipe') {
       setCreateRecipeOpen(true);
+    }
+    if (action === 'recipes') {
+      navigate('/recipes');
     }
   };
 
@@ -24,7 +29,10 @@ function App() {
           onOpen={() => setCreateRecipeOpen(true)}
           onClose={() => setCreateRecipeOpen(false)}
         />
-        <Recipes/> 
+        <Routes>
+          <Route path="/" element={<Navigate to="/recipes" replace />} />
+          <Route path="/recipes" element={<Recipes />} />
+        </Routes>
       </div>
     </>
   )
